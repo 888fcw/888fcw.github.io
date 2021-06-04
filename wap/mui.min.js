0d948ff0427b4c6918084b3f35bc3f3eccc7a901
888fcw <85339177+888fcw@users.noreply.github.com>
2021-06-05 00:30:01 +0800

    Create mui.min.js

---
+++ wap/mui.min.js
@@ -0,0 +1,116 @@
+
+
+<!DOCTYPE html>
+<!-- saved from url=(0034)https://888fcw.com -->
+<html id="cphtml" style="font-size: 104.167px;"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+<title></title>
+<script src="mui.min.js" type="text/javascript"></script>
+<link href="mui.min.css" rel="stylesheet">
+<link rel="stylesheet" href="common.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body class="mui-ios mui-ios-11 mui-ios-11-0">
+<header class="mui-bar mui-bar-nav">
+<h1 class="mui-title">福彩网&nbsp; 欢迎您的加入</h1>
+</header>
+<div id="slider" style="position: relative; overflow: hidden;">
+<ul class="slides clearfix" style="position: relative; width: 400%;">
+<li style="width: 25%; float: left;"><img class="responsive" src="lunbo1.png"></li>
+<li style="width: 25%; float: left;"><img class="responsive" src="lunbo2.png"></li>
+<li style="width: 25%; float: left;"><img class="responsive" src="lunbo3.png"></li>
+</ul>
+<ul class="controls" style="cursor: pointer;">
+<li style="position: absolute; margin-top: -36px;"><img src="prev.png" alt="previous"></li>
+<li style="position: absolute; margin-top: -36px;"><img src="next.png" alt="next"></li>
+</ul>
+<ul class="pagination" style="position: relative; left: 50%; bottom: 20px; margin: 0px 0px 0px -46.5px;">
+<li class="active" style="margin-right: 15px; float: left; cursor: pointer; width: 12px; height: 12px; border-radius: 9999px;"></li>
+<li style="margin-right: 15px; float: left; cursor: pointer; width: 12px; height: 12px; border-radius: 9999px;"></li>
+<li style="margin-right: 15px; float: left; cursor: pointer; width: 12px; height: 12px; border-radius: 9999px;"></li>
+</ul>
+</div>
+<script src="jquery-2.1.1.min.js" type="text/javascript"></script>
+<script src="easySlider.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$(function() {
+		$("#slider").easySlider( {
+			slideSpeed: 500,
+			paginationSpacing: "15px",
+			paginationDiameter: "12px",
+			paginationPositionFromBottom: "20px",
+			slidesClass: ".slides",
+			controlsClass: ".controls",
+			paginationClass: ".pagination"					
+		});
+	});
+</script>
+<div class="mui-content">
+
+<div class="topcon">
+<ul>
+    <div class="xianlulist">
+<div class="xianlutit">官方线路</div>
+<div class="mui-clearfix"></div>
+<div class="xianludot"></div>
+<div class="mui-clearfix"></div>
+<ul>
+<li class="mui-col-xs-4 mui-col-sm-4 mui-pull-left">
+<a href="https://891fcw.com/" target="_blank"><i class="dlxianlu"></i>大陆线路</a>
+</li>
+<li class="mui-col-xs-4 mui-col-sm-4 mui-pull-left">
+<a href="https://890fcw.com/" target="_blank"><i class="twxianlu"></i>澳门线路</a>
+</li>
+<li class="mui-col-xs-4 mui-col-sm-4 mui-pull-left">
+<a href="https://894fcw.com" class="czzx" target="_blank"><i class="hkxianlu"></i>香港线路</a>
+</li>
+<div class="mui-clearfix"></div>
+</ul>
+<ul>
+<li class="mui-col-xs-4 mui-col-sm-4 mui-pull-left">
+<a href="https://895fcw.com" target="_blank"><i class="rbxianlu"></i>台湾线路</a>
+</li>
+<li class="mui-col-xs-4 mui-col-sm-4 mui-pull-left">
+<a href="https://896fcw.com" target="_blank"><i class="hwxianlu"></i>美国线路</a>
+</li>
+<li class="mui-col-xs-4 mui-col-sm-4 mui-pull-left">
+<a href="https://kefu.easemob.com/webim/im.html?configId=c566ecef-c052-4818-b5f9-5e72e7357fc1" target="_blank"><i class="amxianlu"></i>在线客服</a>  <!-- 客服链接修改 --!>
+</li>
+<div class="mui-clearfix"></div>
+</ul>
+<ul>
+<li class="mui-col-xs-4 mui-col-sm-4 mui-pull-left">
+<a href="https://888fcw.org" target="_blank"><i class="zajindan"></i>开奖网</a>
+</li>
+<li class="mui-col-xs-4 mui-col-sm-4 mui-pull-left">
+<a href="https://v888kj.com" target="_blank"><i class="dazhuanpan"></i>PC预测</a>
+</li>
+<li class="mui-col-xs-4 mui-col-sm-4 mui-pull-left">
+<a href="http://mldy.xyz" target="_blank"><i class="hongbao"></i>在线电影</a>
+</li>
+<div class="mui-clearfix"></div>
+</ul>
+</div>
+<li class="mui-col-xs-6 mui-col-sm-6 mui-pull-left">
+<div class="slidercon">
+</div></li><li class="mui-col-xs-6 mui-col-sm-6 mui-pull-left">
+<h3>手机APP</h3>
+<h4>手机游戏 快捷方便</h4>
+<h4>点击下载 注册登录</h4>
+<!--<div class="erweima"><img src="erweima.png"></div>--!>
+<div class="downlink">
+<a href="https://m.891fcw.com/#/download" target="_blank" class="ios"><i class="iconfont"></i> 苹果版下载</a>
+<div class="mui-clearfix"></div>
+<a href="https://m.891fcw.com/#/download" target="_blank" class="android"><i class="iconfont"></i> 安卓版下载</a>
+</div>
+</li>
+<div class="mui-clearfix"></div>
+
+<div class="footer">
+<p>版权所有 2008-2020 © 福彩网</p>
+<center><script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1277980848&web_id=1277980848"></script></center>
+</div>
+</ul></div>
+
+
+</div><div id="syno-notification-is-installed"></div><div id="Pinbox-extension-installed"></div></body></html>
